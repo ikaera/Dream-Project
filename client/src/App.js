014c0c74b9492b35cdb4cea0ad16f8023cefdfe0
ikaera <ikaera@gmail.com>
2023-05-18 16:35:19 -0400

    add Router, Routes and Route tags.

--- client/src/App.js
+++ client/src/App.js
@@ -44,20 +44,26 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <StoreProvider>
+            {/* <Routes>
+              <Route> */}
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+            {/* </Route>
+            </Routes> */}
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
